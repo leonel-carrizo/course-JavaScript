bd4d3ff64a69d3a3d1ddc6baa8f465dac23ca240
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-10-11 16:22:38 +0100

    agrego archivo para practicar git mientras aprendo javaScript

---
+++ ClaseJS2/Sintaxis.js
@@ -0,0 +1,22 @@
+/* Tipado en js = Tipado inferido. Al definir una variable se define de forma general. Ej.
+        a = 1 esta variabell seria de tipo número.
+    No se necesita declarar el tipo de variable. (Boleano, número, string,).
+
+    Tpos:
+        Primitivos: -Number: var a = 1
+                    -String: var str = "String"
+                    -Boolean: var bool = true
+                    -Null: Espacio vacio de memoria. var nul = null
+                    -Undefined: No esta asignada a una parte de memoria. var und = undefined.
+
+        Objetos: -Tipos
+                - arrays
+                - listas
+
+        Variables:
+                -var
+                -let
+                -const
+                -typeof
+*/
+
