c9fa976601909afafbcd6906c1fe4ef0af25151e
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-10-22 11:25:49 +0100

    clase notación en javaScript

---
+++ ClaseJS2/notación.js
@@ -0,0 +1,44 @@
+/*
+// Notación
+
+// ; -> cada linea al final debe estar marcada, delimita el final de una linea.
+
+al día de hoy ya noes necesario se ha eliminado. 
+
+const b = 4; 
+
+// . -> se utiliza en los objetos. para acceder a los atributos.
+
+movil.altura
+
+// [ ] -> para definir listas, arreglos, arrays, o para acceder a algun valor de estos.
+
+const ar = [1, 2, 3, 4];
+
+console.log(ar[2]);
+
+// ( ) -> para definir funsiones
+
+funtion suma = (a, b) {
+
+cuerpo de función
+
+}
+
+// { } -> Para objetos, funsiiones y estructuras de control
+
+const movil = {
+
+anchura: 5,
+
+alrtura: 10
+
+}
+
+if (true) {
+
+toda la condicion
+
+}
+
+*/
