7bf2f07867ba438c057b15996da14f41d480b7ca
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-10-22 11:14:20 +0100

    clase variables

--- ClaseJS2/Sintaxis.js
+++ ClaseJS2/Sintaxis.js
@@ -1,13 +1,15 @@
 /* Tipado en js = Tipado inferido. Al definir una variable se define de forma general. Ej.
         a = 1 esta variabell seria de tipo número.
+        a = "hola" la variable sería tipo cadena.
     No se necesita declarar el tipo de variable. (Boleano, número, string,).
 
     Tpos:
         Primitivos: -Number: var a = 1
                     -String: var str = "String"
                     -Boolean: var bool = true
-                    -Null: Espacio vacio de memoria. var nul = null
-                    -Undefined: No esta asignada a una parte de memoria. var und = undefined.
+                    -Null: Espacio vacio de memoria, significa "nada" y es intencional. var nul = null
+                    -Undefined: No esta asignada a una parte de memoria, se declara la variable pero no se le da un valor. var nombre = undefined.
+                    -Relación entre null, undefined, false y 0: todos son Falsy
 
         Objetos: -Tipos
                 - arrays
@@ -19,4 +21,27 @@
                 -const
                 -typeof
 */
+
+// Number
+4;
+
+// strting
+("hola mundo");
+("hola mundo");
+`hola mundo`;
+
+//booleanos
+true;
+false;
+
+//null and undefined
+null;
+undefined;
+
+//null, undefined, false, 0
+if (0) {
+	console.log("cumple");
+} else {
+	console.log("no cumple");
+}
 
---
+++ ClaseJS2/variables.js
@@ -0,0 +1,36 @@
+/*Una variable es una especie de caja en la que se almacena un valor y este valor puede cambiar es mutrable.
+
+La diferencia entre let y var es que var afecta todo el código y let solo afecta el bloque en el cual ha sido declarado.
+
+var variable;
+
+let variablelet;
+
+var a = 1;
+
+console.log(a);
+
+var a= 4;
+
+console.log(a);
+
+let b = 3;
+
+console.log(a);
+
+let b = 5;
+
+console.log(a);
+
+const constantes;
+
+const contante = "hola soy una constante"
+
+console.log(constante)
+
+si tratamos de cambiar el valor de la constante dara ERROR ya que es un dato inmmutable.
+
+typeof: nos indica el tipo de variable
+
+console.log(typeof "hola") ->string
+*/
