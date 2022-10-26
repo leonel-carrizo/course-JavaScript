f3f948409a12e041bda1b8bd463aea11ca46565e
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-10-26 12:56:34 +0100

    Ejercicio Clase 2 Listas, Objetos y fechas

---
+++ ClaseJS2/Ejercicio2.js
@@ -0,0 +1,13 @@
+const lista = [
+	"Leonel Carrizo",
+	35,
+	true,
+	new Date(1987, 4, 12),
+	{
+		libro: "El Progreso del Peregrino",
+		autor: "John Bunyan",
+		fecha: 1678,
+		url: "https://www.amazon.co.uk/El-progreso-del-peregrino-Spanish-ebook/dp/B08YYV6HQ9",
+	},
+];
+console.log(lista);
