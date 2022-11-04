d21cd2f043de332fb406e87cb8974602b73193e2
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-11-04 22:29:49 +0000

    integro dependencia Winston y errores

--- ClaseJS9_GestionErrores/Ejercicio_Sesion9/index.js
+++ ClaseJS9_GestionErrores/Ejercicio_Sesion9/index.js
@@ -1,12 +1,29 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+	level: "error",
+	format: winston.format.json(),
+	defaultMeta: { service: "user-service" },
+	transports: [
+		//
+		// - Write all logs with importance level of `error` or less to `error.log`
+		// - Write all logs with importance level of `info` or less to `combined.log`
+		//
+		new winston.transports.File({ filename: "error.log", level: "error" }),
+		//new winston.transports.File({ filename: "combined.log" }),
+	],
+});
+
 try {
-    const miFuncion = (a) => {
-        if (a <= 5) {
-            return  a = `El numero es ${a}`
-        }
-        throw new Error('El numero debe ser menor de 5')
-    }
-    const a = 6;
-    console.log(miFuncion(a))
+	const miFuncion = (a) => {
+		if (a <= 5) {
+			return (a = `El numero es ${a}`);
+		}
+		throw new Error("El numero debe ser menor de 5");
+	};
+	const a = 9;
+	console.log(miFuncion(a));
 } catch (e) {
-    console.error(`Valor del ${e}`);
+	logger.error(`Valor del ${e}`);
+	console.error(`Valor del ${e}`);
 }
