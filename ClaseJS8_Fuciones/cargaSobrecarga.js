bef0958bb73e58f0bd9e298ff654fc406932f878
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-11-04 14:04:51 +0000

    Carga y sobrecarga de funcions - stack overfslow

---
+++ ClaseJS8_Fuciones/cargaSobrecarga.js
@@ -0,0 +1,17 @@
+//Carga y Sobrecarga de funciones
+
+// Carga de funciones
+function saludar() {
+	hola();
+}
+
+function hola() {
+	console.log("Hola mundo cruel");
+}
+saludar();
+
+// 1. global()
+//2. saludar() global()
+//3. hola() saluda() global()
+//4. saludar() global()
+//5. global()
