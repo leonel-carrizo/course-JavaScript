6f94ed56959edfdf27f21e072e19c3eba9441777
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-11-11 02:16:43 +0000

    Corrijo ejercicio

--- clasejs15-persistencia-de-datos/ejercicio-sesion15-persistencia-de-datos/index.js
+++ clasejs15-persistencia-de-datos/ejercicio-sesion15-persistencia-de-datos/index.js
@@ -1,11 +1,11 @@
 const nombre = "Leonel"
 const apellido = "Carrizo"
-const persona = { nombre: nombre, apellido: apellido}
+const persona = { nombre: nombre, apellido: apellido }
 
 // sessionStorage.setItem("persona", persona)
 
 // localStorage.setItem("persona", persona)
 
-document.cookie = "nombreCookie=LeonelCookie"
 
-document.cookie = "nombreCaducidad=nombreCookie; expires=" + new Date(2022, 11, 11,0,2).toUTCString()
+const now = new Date()
+document.cookie = `persona=${JSON.stringify(persona)}; expires=${new Date(now.getTime() + 2 * 600)}`
